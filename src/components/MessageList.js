c735c983b27e2a1b77c16c5b5b045ff6ecdfd539
Daniel Seiders <DanSeiders@dseid.lan>
2020-12-09 23:30:17 -0500

    basic dashboard started, login page started

---
+++ src/components/MessageList.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function MessageList() {
+    return (
+        <div className='dashboard-messages-container'>
+            <h2>MESSAGES</h2>
+            <h5>User11423</h5>
+            <h5>User4667</h5>
+            <h5>User83634</h5>
+            <h5>User163445</h5>
+            <h5>User4354</h5>
+            <h5>User727</h5>
+            <h5>User2721</h5>
+            <h5>User7247</h5>
+        </div>
+    )
+}
