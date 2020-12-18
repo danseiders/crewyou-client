f2562535ac4ae3af9298811c1eae2be7aab9f56f
danseiders <dseiders333@gmail.com>
2020-12-18 00:32:47 -0500

    styled and added readme

--- src/components/CrewList.js
+++ src/components/CrewList.js
@@ -6,7 +6,7 @@ export default function CrewList(props) {
             <h1>Crew List</h1>
             <div>
                 {props.crew.map((crew)=>
-                <div className='dash-crew-container' key={crew.id}>
+                <div className='dash-crew-container' key={crew.id} id={crew.id}>
                     <img className='dash-crew-img' src={crew.imgURL} alt='member' />
                     <div>
                         <h4>{crew.firstName} {crew.lastName} - {crew.airport}<br></br>{crew.position1}</h4>
