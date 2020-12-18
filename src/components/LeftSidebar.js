891c26f0b614c672d7afedf41175c751e9551f72
danseiders <dseiders333@gmail.com>
2020-12-17 20:40:55 -0500

    added styling to dashboard, crew list and left sidebar

--- src/components/LeftSidebar.js
+++ src/components/LeftSidebar.js
@@ -3,11 +3,11 @@ import React from 'react'
 export default function LeftSidebar(props) {
     return (
         <div className='dashboard-sidebar-container'>
-                <p onClick={props.handleClick} id='profileRender'>Profile</p>
-                <p onClick={props.handleClick} id='crewRender'>Crew</p>
-                <p onClick={props.handleClick} id='messagesRender'>Messages</p>
-                <p onClick={props.handleClick} id='managersRender'>Managers</p>
-                <p onClick={props.handleClick} id='jobBoardRender'>Job Board</p>
+                <p className='sidebar-link' onClick={props.handleClick} id='profileRender'>Profile</p>
+                <p className='sidebar-link' onClick={props.handleClick} id='crewRender'>Crew</p>
+                <p className='sidebar-link' onClick={props.handleClick} id='messagesRender'>Messages</p>
+                <p className='sidebar-link' onClick={props.handleClick} id='managersRender'>Managers</p>
+                <p className='sidebar-link' onClick={props.handleClick} id='jobBoardRender'>Job Board</p>
                 
         </div>
     )
--- src/components/ProfileShow.js
+++ src/components/ProfileShow.js
@@ -4,23 +4,20 @@ import { Link } from 'react-router-dom'
 export default function ProfileShow(props) {
     
     return (
-        <div className='dashboard-profile-container'>
+        <div>
             <div className='profile-header'>
-                <img src={props.user.data.imgURL} alt='user'></img>
+                <img className='profile-img' src={props.user.data.imgURL} alt='user'></img>
                 <h2>{props.user.data.firstName} {props.user.data.lastName}</h2>
                 <h3>{props.user.data.airport}</h3>
             </div>
             <div className='profile-details'>
                 <h4>About: {props.user.data.about}</h4>
-                <h4>Position 1: {props.user.data.position1}</h4>
-                <h4>Position 2: {props.user.data.position2}</h4>
-                <h4>Position 3: {props.user.data.position3}</h4>
-                <h4>Position 4: {props.user.data.position4}</h4>
+                <h4>Positions: {props.user.data.position1}, {props.user.data.position2},  {props.user.data.position3}, {props.user.data.position4}</h4>
                 {props.user.data.touring ? <h4>Touring: Yes </h4> : <h4>Touring: No </h4>}
                 <h4>Available on: {props.user.data.availability}</h4>
             </div>
             <div>
-                { props.user.data.id > 0 ? <Link to='/profile/edit' ><button>Edit Profile</button></Link> : <Link to='profile/new'><button>Create Profile</button></Link> }
+                { props.user.data.id > 0 ? <Link to='/profile/edit' ><button className='btn-dark'>Edit Profile</button></Link> : <Link to='profile/new'><button>Create Profile</button></Link> }
             </div>
         </div>
     )
